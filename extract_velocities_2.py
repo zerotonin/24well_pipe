e8d2dea6e2e77e0cd9fdc00962b2ac52c3645504
alexrhmatthews <rednaxela.matthews@gmail.com>
2024-03-21 18:28:23 +1300

    Frons and com calculation

---
+++ extract_velocities_2.py
@@ -0,0 +1,1 @@
+filepath = r"c:\Users\Lindsay\Documents\alexander\Honors\data_with_coords\Cas9-B1\P1000355\ts01DLC_resnet50_single_tadpoleNov23shuffle1_1030000.csv"
--- extract_velocity.py
+++ extract_velocity.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 import os
 
-
-filepath = r"D:\split_videos\Cas9-B1\P1000355\ts01DLC_resnet50_single_tadpoleNov23shuffle1_1030000.csv"
+filepath = r"c:\Users\Lindsay\Documents\alexander\Honors\data_with_coords\Cas9-B1\P1000355\ts01DLC_resnet50_single_tadpoleNov23shuffle1_1030000.csv"
+filepath =# r"D:\split_videos\Cas9-B1\P1000355\ts01DLC_resnet50_single_tadpoleNov23shuffle1_1030000.csv"
 #path_to_data = r'd:\tracking_locations\ts04DLC_resnet50_single_tadpoleNov23shuffle1_1030000.csv'
 
 tracked_data= pd.read_csv(filepath)
